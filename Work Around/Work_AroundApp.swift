cc17f5ba867e3218ec045a4ab71b6581ee82a1a4
qcoley <qcoley@uab.edu>
2022-10-26 22:08:01 -0500

    Initial Commit

---
+++ Work Around/Work_AroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Work_AroundApp.swift
+//  Work Around
+//
+//  Created by Intrinsic Q on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct Work_AroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
